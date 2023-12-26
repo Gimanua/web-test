06564d8add0aa6e5f2790123cc74836c2786d573
Adrian Klasson <gimanua.dota2@gmail.com>
2023-12-26 21:35:30 +0100

    Test input styling

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-console.log('Test')
+/** @type {HTMLButtonElement} */
+const openDialogButtonElement = document.querySelector('#open-dialog')
+
+/** @type {HTMLDialogElement} */
+const dialogElement = document.querySelector('#dialog')
+/** @type {HTMLInputElement} */
+const hasPartsElement = document.querySelector('#has-parts')
+
+/** @type {HTMLTableElement} */
+const partsElement = document.querySelector('#parts')
+/** @type {HTMLTemplateElement} */
+const partTemplateElement = document.querySelector('#part-template')
+/** @type {HTMLInputElement} */
+const newPartNameElement = document.querySelector('#parts .name')
+/** @type {HTMLInputElement} */
+const newPartValue1Element = document.querySelector('#parts .value-1')
+/** @type {HTMLInputElement} */
+const newPartValue2Element = document.querySelector('#parts .value-2')
+/** @type {HTMLButtonElement} */
+const newPartAddButtonElement = document.querySelector('#parts .add-button')
+
+openDialogButtonElement.addEventListener('click', () => {
+  dialogElement.showModal()
+})
+
+dialogElement.addEventListener('close', e => {
+  console.log(`dialog closed, returnValue: type ${typeof dialogElement.returnValue} value ${dialogElement.returnValue}`)
+})
+
+hasPartsElement.addEventListener('change', () => {
+  if (hasPartsElement.checked) {
+    partsElement.classList.remove('hide')
+  } else {
+    partsElement.classList.add('hide')
+  }
+})
+
+newPartAddButtonElement.addEventListener('click', () => {
+  const partElement = partTemplateElement.content.cloneNode(true)
+
+  const nameElement = partElement.querySelector('.name')
+  const value1Element = partElement.querySelector('.value-1')
+  const value2Element = partElement.querySelector('.value-2')
+
+  partsElement.querySelector('tbody').append(partElement)
+
+  nameElement.value = partsElement.querySelector('.new-part .name').value
+  value1Element.value = partsElement.querySelector('.new-part .value-1').value
+  value2Element.value = partsElement.querySelector('.new-part .value-2').value
+})
